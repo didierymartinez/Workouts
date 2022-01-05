6de90169ae0490ca6fb964d4f1cea810f8a6c6ad
Didier Yamit Martinez <didierymartinez@gmail.com>
2022-01-05 15:03:33 -0500

    Index y proyecto

---
+++ React/Curso_FH/memo/src/App.js
@@ -0,0 +1,12 @@
+import { Padre } from './07-tarea-memo/Padre';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Padre></Padre>
+    </div>
+  );
+}
+
+export default App;
